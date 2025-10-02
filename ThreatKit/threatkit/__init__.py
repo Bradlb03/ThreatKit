83db3e6eed85a756c1fcda6d8df489fcde6150e9
PBoekhout <pbfmm@umsystem.edu>
2025-10-02 09:17:45 -0500

    adding new files format

---
+++ ThreatKit/threatkit/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    # Blueprints
+    from .main.routes import bp as main_bp
+    from .password.routes import bp as password_bp
+    from .malware.routes import bp as malware_bp
+    from .emailcheck.routes import bp as email_bp
+    from .linkcheck.routes import bp as link_bp
+
+    app.register_blueprint(main_bp)                
+    app.register_blueprint(password_bp, url_prefix="/password")
+    app.register_blueprint(malware_bp,  url_prefix="/malware")
+    app.register_blueprint(email_bp,    url_prefix="/email")
+    app.register_blueprint(link_bp,     url_prefix="/link")
+
+    return app
