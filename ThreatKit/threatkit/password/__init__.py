bc9d6cce1e66f63ce1ec1624d68a78dfceed87c7
Michael Loutos <michaelloutos@mwc-121114.dhcp.missouri.edu>
2025-12-04 15:11:34 -0600

    Small changes

--- ThreatKit/threatkit/password/__init__.py
+++ ThreatKit/threatkit/password/__init__.py
@@ -13,15 +13,9 @@ def create_app():
     from .password_.routes import bp as password_bp
     app.register_blueprint(password_bp)
 
-    # You can register other toolkits here too:
-    # from .emailcheck.routes import email_bp; app.register_blueprint(email_bp)
-    # from .url_scanner.routes import url_bp; app.register_blueprint(url_bp)
-    # from .malware.routes import malware_bp; app.register_blueprint(malware_bp)
-
-    # Home route (optional): redirect to index or password page
     @app.get("/")
     def _root():
         from flask import render_template
-        return render_template("index.html")  # you already have index.html
+        return render_template("index.html") 
 
     return app
